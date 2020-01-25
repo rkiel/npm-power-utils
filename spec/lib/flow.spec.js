49ad374423dfb1d69b44b556ea7b3919f77578b0
rkiel <cygnus@pobox.com>
2020-01-25 12:09:35 -0500

    basics: added input output and catch

--- spec/lib/flow.spec.js
+++ spec/lib/flow.spec.js
@@ -3,13 +3,16 @@ const lib = pathTo("lib/flow").lib;
 
 describe("flow", () => {
   it("should work", () => {
-    expect(lib.flow({})).toEqual(null);
+    expect(lib.flow({})).toEqual(undefined);
+  });
+  it("should work", () => {
+    expect(lib.flow({ output: 400 })).toEqual(400);
   });
   it("should work", () => {
     const double = a => a * 2;
     expect(
       lib.flow({
-        in: 2,
+        input: 2,
         now: double
       })
     ).toEqual(4);
@@ -18,17 +21,26 @@ describe("flow", () => {
     const double = a => a * 2;
     expect(
       lib.flow({
-        in: [2],
+        input: 2,
+        now: double,
+        output: 400
+      })
+    ).toEqual(400);
+  });
+  it("should work", () => {
+    const double = a => a * 2;
+    expect(
+      lib.flow({
+        input: [2],
         now: [double]
       })
     ).toEqual(4);
   });
   it("should work", () => {
     const add = (a, b) => a + b;
-    const double = a => a * 2;
     expect(
       lib.flow({
-        in: [1, 2],
+        input: [1, 2],
         now: [add]
       })
     ).toEqual(3);
@@ -38,7 +50,7 @@ describe("flow", () => {
     const double = a => a * 2;
     expect(
       lib.flow({
-        in: [1, 2],
+        input: [1, 2],
         now: [add, double]
       })
     ).toEqual(6);
