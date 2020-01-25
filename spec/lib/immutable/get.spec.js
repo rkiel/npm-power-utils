bd88951a6a55a02e8816d32a22971b2622a09457
rkiel <cygnus@pobox.com>
2020-01-25 17:52:50 -0500

    basics: more refactoring

--- spec/lib/immutable/get.spec.js
+++ spec/lib/immutable/get.spec.js
@@ -6,45 +6,4 @@ describe("get", () => {
   beforeEach(() => (object = {}));
   beforeEach(() => (value = 1));
   beforeEach(() => (defaultValue = 1024));
-
-  it("should work", () => {
-    object = undefined;
-    expect(lib.get(object, "one")).toEqual(undefined);
-    expect(lib.get(object, "one", defaultValue)).toEqual(defaultValue);
-  });
-  it("should work", () => {
-    object = null;
-    expect(lib.get(object, "one")).toEqual(undefined);
-    expect(lib.get(object, "one", defaultValue)).toEqual(defaultValue);
-  });
-  it("should work", () => {
-    object = {};
-    expect(lib.get(object, "one")).toEqual(undefined);
-    expect(lib.get(object, "one", defaultValue)).toEqual(defaultValue);
-  });
-  it("should work", () => {
-    object = { one: 1 };
-    expect(lib.get(object, "one")).toEqual(1);
-    expect(lib.get(object, "one", defaultValue)).toEqual(1);
-  });
-  it("should work", () => {
-    object = { one: 1 };
-    expect(lib.get(object, "one.two")).toEqual(undefined);
-    expect(lib.get(object, "one.two", defaultValue)).toEqual(defaultValue);
-  });
-  it("should work", () => {
-    object = { one: 1, two: 2 };
-    expect(lib.get(object, "one.two")).toEqual(undefined);
-    expect(lib.get(object, "one.two", defaultValue)).toEqual(defaultValue);
-  });
-  it("should work", () => {
-    object = { one: { two: 2 } };
-    expect(lib.get(object, "one.two")).toEqual(2);
-    expect(lib.get(object, "one.two", defaultValue)).toEqual(2);
-  });
-  it("should work", () => {
-    object = { one: { three: 3 } };
-    expect(lib.get(object, "one.two")).toEqual(undefined);
-    expect(lib.get(object, "one.two", defaultValue)).toEqual(defaultValue);
-  });
 });
---
+++ spec/lib/immutable/shared/get.spec.js
@@ -0,0 +1,52 @@
+const pathTo = x => require("../../../../" + x);
+const lib = pathTo("lib/immutable/shared/get").lib;
+
+describe("get", () => {
+  let object, value, defaultValue;
+  beforeEach(() => (object = {}));
+  beforeEach(() => (value = 1));
+  beforeEach(() => (defaultValue = 1024));
+
+  const s = x => x.split(".");
+
+  it("should work", () => {
+    object = undefined;
+    expect(lib.get(object, s("one"))).toEqual(undefined);
+    expect(lib.get(object, s("one"), defaultValue)).toEqual(defaultValue);
+  });
+  it("should work", () => {
+    object = null;
+    expect(lib.get(object, s("one"))).toEqual(undefined);
+    expect(lib.get(object, s("one"), defaultValue)).toEqual(defaultValue);
+  });
+  it("should work", () => {
+    object = {};
+    expect(lib.get(object, s("one"))).toEqual(undefined);
+    expect(lib.get(object, s("one"), defaultValue)).toEqual(defaultValue);
+  });
+  it("should work", () => {
+    object = { one: 1 };
+    expect(lib.get(object, s("one"))).toEqual(1);
+    expect(lib.get(object, s("one"), defaultValue)).toEqual(1);
+  });
+  it("should work", () => {
+    object = { one: 1 };
+    expect(lib.get(object, s("one.two"))).toEqual(undefined);
+    expect(lib.get(object, s("one.two"), defaultValue)).toEqual(defaultValue);
+  });
+  it("should work", () => {
+    object = { one: 1, two: 2 };
+    expect(lib.get(object, s("one.two"))).toEqual(undefined);
+    expect(lib.get(object, s("one.two"), defaultValue)).toEqual(defaultValue);
+  });
+  it("should work", () => {
+    object = { one: { two: 2 } };
+    expect(lib.get(object, s("one.two"))).toEqual(2);
+    expect(lib.get(object, s("one.two"), defaultValue)).toEqual(2);
+  });
+  it("should work", () => {
+    object = { one: { three: 3 } };
+    expect(lib.get(object, s("one.two"))).toEqual(undefined);
+    expect(lib.get(object, s("one.two"), defaultValue)).toEqual(defaultValue);
+  });
+});
