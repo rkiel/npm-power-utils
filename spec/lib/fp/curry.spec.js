e1cdad9c020edf88ce47ee375fc3014d50daf93a
rkiel <cygnus@pobox.com>
2020-01-29 12:00:09 -0500

    basics: added curry

---
+++ spec/lib/fp/curry.spec.js
@@ -0,0 +1,28 @@
+const pathTo = x => require("../../../" + x);
+const lib = pathTo("lib/fp/curry").lib;
+
+describe("curry", () => {
+  it("should work with no arguments", () => {
+    const f = lib.curry(() => 0);
+    expect(f()).toEqual(0);
+  });
+  it("should work with one argument", () => {
+    const f = lib.curry(a => a);
+    expect(f(2)()).toEqual(2);
+  });
+  it("should work with two arguments", () => {
+    const f = lib.curry((a, b) => a * b);
+    expect(f(2, 3)).toEqual(6);
+    expect(f(2)(3)).toEqual(6);
+  });
+  it("should work with three arguments", () => {
+    const f = lib.curry((a, b, c) => a * b * c);
+    expect(f(2, 3, 4)).toEqual(24);
+    expect(f(2)(3)(4)).toEqual(24);
+  });
+  it("should work with four arguments", () => {
+    const f = lib.curry((a, b, c, d) => a * b * c * d);
+    expect(f(2, 3, 4, 5)).toEqual(120);
+    expect(f(2)(3)(4)(5)).toEqual(120);
+  });
+});
