31cc9554aea5d5bbcb7d48227199ea31d9e23dd5
rkiel <cygnus@pobox.com>
2020-01-31 00:12:38 -0500

    basics: added eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,10 @@ module.exports = {
   env: {
     browser: true,
     commonjs: true,
-    es6: true
+    es6: true,
+    node: true
   },
-  extends: ["eslint:recommended"],
+  extends: "eslint:recommended",
   globals: {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly"
@@ -13,6 +14,7 @@ module.exports = {
     ecmaVersion: 2018
   },
   rules: {
+    "linebreak-style": ["error", "unix"],
     quotes: ["error", "single"],
     semi: ["error", "always"],
     "no-unused-vars": [
