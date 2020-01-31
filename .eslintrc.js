4190cbb0ce18fde72c9ac3a32f1bd7b3faf87ff2
rkiel <cygnus@pobox.com>
2020-01-31 00:07:21 -0500

    basics: eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
+  },
+  extends: ["eslint:recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {
+    quotes: ["error", "single"],
+    semi: ["error", "always"],
+    "no-unused-vars": [
+      "error",
+      { vars: "all", args: "all", ignoreRestSiblings: false }
+    ]
+  }
+};
