5dafb3300e5048c9bd4c3e18e918106057b94f70
rkiel <cygnus@pobox.com>
2026-03-12 11:23:05 -0400

    more tests

--- spec/lib/state/Immutable.spec.js
+++ spec/lib/state/Immutable.spec.js
@@ -1,64 +1,174 @@
 const Immutable = require('../../../lib/state/Immutable');
 
 describe('Immutable', () => {
-  describe('init', () => {
-    it('should work', () => {
-      const state = {};
-      const im = new Immutable(state, 'geddy');
-      expect(im.init({ one: 1 })).toEqual({ geddy: { one: 1 } });
-      expect(state).toEqual({});
-    });
+  let state, im;
 
-    it('should work', () => {
-      const state = { geddy: { two: 2 } };
-      const im = new Immutable(state, 'geddy');
-      expect(im.init({ one: 1 })).toEqual({ geddy: { one: 1 } });
-      expect(state).toEqual({ geddy: { two: 2 } });
-    });
-  });
+  describe('WHEN state is empty', () => {
+    beforeEach(() => (state = {}));
+    beforeEach(() => (im = new Immutable(state, 'geddy')));
+    afterEach(() => expect(state).toEqual({}));
 
-  describe('assign', () => {
-    it('should work', () => {
-      const state = { geddy: { two: 2 } };
-      const im = new Immutable(state, 'geddy');
-      expect(im.assign({ one: 1 })).toEqual({ geddy: { two: 2, one: 1 } });
-      expect(state).toEqual({ geddy: { two: 2 } });
-    });
-  });
-
-  describe('get', () => {
-    it('should work', () => {
-      const alex = { alpha: 'a' };
-      const geddy = { beta: 'b', one: { gamma: 'g' } };
-      const state = { alex, geddy };
-      const im = new Immutable(state, 'geddy');
-      expect(im.get('one', 1)).toEqual({ gamma: 'g' });
-      expect(im.get('two')).toEqual(undefined);
-      expect(im.get('two', 1)).toEqual(1);
-      expect(im.get('one.two', 2)).toEqual(2);
-      expect(im.get('one.two.three', 3)).toEqual(3);
-      expect(im.get('beta', 1)).toEqual('b');
-      expect(im.get('two', 1)).toEqual(1);
-      expect(im.get()).toEqual(geddy);
-    });
-  });
-
-  describe('set', () => {
-    it('should work', () => {
-      const alex = { alpha: 'a' };
-      const geddy = { beta: 'b', one: { gamma: 'g' } };
-      const state = { alex, geddy };
-      const im = new Immutable(state, 'geddy');
-      expect(im.set('one', 11)).toEqual({ alex, geddy: { beta: 'b', one: 11 } });
-      expect(im.set('two', 22)).toEqual({ alex, geddy: { beta: 'b', one: { gamma: 'g' }, two: 22 } });
-      expect(im.set('beta', 'B')).toEqual({ alex, geddy: { beta: 'B', one: { gamma: 'g' } } });
-      expect(im.set('one.gamma', 'G')).toEqual({ alex, geddy: { beta: 'b', one: { gamma: 'G' } } });
-      expect(im.set('one.two', 222)).toEqual({ alex, geddy: { beta: 'b', one: { gamma: 'g', two: 222 } } });
-      expect(im.set('one.two.three', 3333)).toEqual({
-        alex,
-        geddy: { beta: 'b', one: { gamma: 'g', two: { three: 3333 } } }
+    describe('get', () => {
+      it('should define nothing by default', () => {
+        expect(im.get()).toBeUndefined();
       });
-      expect(state).toEqual({ alex, geddy });
-    });
-  });
+
+      it('should dereference one level', () => {
+        expect(im.get('one')).toBeUndefined();
+      });
+
+      it('should dereference multiple levels', () => {
+        expect(im.get('one.two.three')).toBeUndefined();
+      });
+
+      it('should dereference one level with a default value', () => {
+        expect(im.get('one', 1)).toEqual(1);
+      });
+
+      it('should dereference multiple levels with a default value', () => {
+        expect(im.get('one.two.three', 3)).toEqual(3);
+      });
+    }); // get
+
+    describe('initialize', () => {
+      it('should initialize an integer', () => {
+        expect(im.init(1)).toEqual({ geddy: 1 });
+      });
+
+      it('should initialize an array', () => {
+        expect(im.init([1])).toEqual({ geddy: [1] });
+      });
+
+      it('should initialize an object', () => {
+        expect(im.init({ one: 1 })).toEqual({ geddy: { one: 1 } });
+      });
+
+      it('should initialize a function', () => {
+        expect(im.init((x) => x + x).geddy(2)).toEqual(4);
+      });
+    }); // initialize
+
+    describe('assign', () => {
+      it('should not assign if not an object', () => {
+        expect(() => im.assign({ one: 1 })).toThrowError('Not an object');
+      });
+    }); // assign
+
+    describe('set', () => {
+      it('should set one level', () => {
+        expect(im.set('one', 1)).toEqual({ geddy: { one: 1 } });
+      });
+
+      it('should set multiple levels', () => {
+        expect(im.set('one.two.three', 3)).toEqual({ geddy: { one: { two: { three: 3 } } } });
+      });
+    }); // set
+  }); // WHEN state is empty
+
+  describe('WHEN state is not empty (simple)', () => {
+    beforeEach(() => (state = { geddy: { two: 2 } }));
+    beforeEach(() => (im = new Immutable(state, 'geddy')));
+    afterEach(() => expect(state).toEqual({ geddy: { two: 2 } }));
+
+    describe('get', () => {
+      it('should the whole value', () => {
+        expect(im.get()).toEqual({ two: 2 });
+      });
+
+      it('should dereference one level', () => {
+        expect(im.get('one')).toBeUndefined();
+      });
+
+      it('should dereference multiple levels', () => {
+        expect(im.get('one.two.three')).toBeUndefined();
+      });
+
+      it('should dereference one level with a default value', () => {
+        expect(im.get('one', 1)).toEqual(1);
+      });
+
+      it('should dereference multiple levels with a default value', () => {
+        expect(im.get('one.two.three', 3)).toEqual(3);
+      });
+    }); // get
+
+    describe('initialize', () => {
+      it('should initialize an integer', () => {
+        expect(im.init(1)).toEqual({ geddy: 1 });
+      });
+
+      it('should initialize an array', () => {
+        expect(im.init([1])).toEqual({ geddy: [1] });
+      });
+
+      it('should initialize an object', () => {
+        expect(im.init({ one: 1 })).toEqual({ geddy: { one: 1 } });
+      });
+
+      it('should initialize a function', () => {
+        expect(im.init((x) => x + x).geddy(2)).toEqual(4);
+      });
+    }); // initialize
+
+    describe('assign', () => {
+      it('should not assign if not an object', () => {
+        expect(im.assign({ one: 1 })).toEqual({ geddy: { two: 2, one: 1 } });
+      });
+    }); // assign
+
+    describe('set', () => {
+      it('should set one level', () => {
+        expect(im.set('two', 22)).toEqual({ geddy: { two: 22 } });
+      });
+
+      it('should set one level', () => {
+        expect(im.set('one', 1)).toEqual({ geddy: { two: 2, one: 1 } });
+      });
+
+      it('should set multiple levels', () => {
+        expect(im.set('one.two.three', 3)).toEqual({ geddy: { two: 2, one: { two: { three: 3 } } } });
+      });
+    }); // set
+  }); // WHEN state is not empty (simple)
+
+  describe('WHEN state is not empty (complex)', () => {
+    let alex, geddy;
+    beforeEach(() => (alex = { alpha: 'a' }));
+    beforeEach(() => (geddy = { beta: 'b', one: { gamma: 'g' } }));
+    beforeEach(() => (state = { alex, geddy }));
+    beforeEach(() => (im = new Immutable(state, 'geddy')));
+    afterEach(() => expect(state).toEqual({ alex, geddy }));
+
+    describe('get', () => {
+      it('should work', () => {
+        expect(im.get('beta', 1)).toEqual('b');
+        expect(im.get('one', 1)).toEqual({ gamma: 'g' });
+      });
+      it('should work', () => {
+        expect(im.get('two')).toEqual(undefined);
+        expect(im.get('two', 1)).toEqual(1);
+        expect(im.get('one.two', 2)).toEqual(2);
+        expect(im.get('one.two.three', 3)).toEqual(3);
+      });
+      it('should work', () => {
+        expect(im.get()).toEqual(geddy);
+      });
+    }); // get
+
+    describe('set', () => {
+      it('should work', () => {
+        expect(im.set('one', 11)).toEqual({ alex, geddy: { beta: 'b', one: 11 } });
+        expect(im.set('beta', 'B')).toEqual({ alex, geddy: { beta: 'B', one: { gamma: 'g' } } });
+      });
+      it('should work', () => {
+        expect(im.set('two', 22)).toEqual({ alex, geddy: { beta: 'b', one: { gamma: 'g' }, two: 22 } });
+        expect(im.set('one.gamma', 'G')).toEqual({ alex, geddy: { beta: 'b', one: { gamma: 'G' } } });
+        expect(im.set('one.two', 222)).toEqual({ alex, geddy: { beta: 'b', one: { gamma: 'g', two: 222 } } });
+        expect(im.set('one.two.three', 3333)).toEqual({
+          alex,
+          geddy: { beta: 'b', one: { gamma: 'g', two: { three: 3333 } } }
+        });
+      });
+    }); // set
+  }); //WHEN state is not empty (complex)
 });
--- spec/lib/state/use.spec.js
+++ spec/lib/state/use.spec.js
@@ -1,11 +1,42 @@
 const use = require('../../../lib/state/use');
 
 describe('use', () => {
-  it('should create missing objects', () => {
+  it('should create missing top-level objects', () => {
     const state = {};
     const newState = use(state, 'geddy', 'alex');
     expect(Object.keys(newState).sort()).toEqual(['alex', 'geddy']);
     expect(Object.keys(state)).toEqual([]);
+  });
+
+  it('should fail getting from a non-object', () => {
+    const state = { integer: 1 };
+    const { integer } = use(state);
+    expect(() => integer.get('one')).toThrowError('Not an object');
+  });
+
+  it('should fail setting to a non-object', () => {
+    const state = { integer: 1 };
+    const { integer } = use(state);
+    expect(() => integer.set('one', 2112)).toThrowError('Not an object');
+  });
+
+  it('should dereference one level without a value', () => {
+    const state = {};
+    const { integer } = use(state, 'integer');
+    expect(integer.get('one')).toEqual(undefined);
+  });
+
+  it('should dereference one level with a value', () => {
+    const state = { integer: 2112 };
+    const { integer } = use(state);
+    expect(integer.get()).toEqual(2112);
+  });
+
+  it('should initialize', () => {
+    const state = { integer: 2112 };
+    const { integer } = use(state);
+    expect(integer.init(90210)).toEqual({ integer: 90210 });
+    expect(state).toEqual({ integer: 2112 });
   });
 
   it('should not create existing objects', () => {
